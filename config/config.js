2feb2e414d37b093c8227ec6bcd9b49b19938ce0
giltamayo <tamayo_144@hotmail.com>
2018-08-09 14:44:45 -0500

    ultima limpieza de codigo

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 const config = {
     version: '1.0',
     port: 3000,
-    'hostname': 'localhost',
+    hostname: 'localhost',
     mongo: 'mongodb://localhost:27017/rest_person',
     bodyLimit: '10kb'
 };
